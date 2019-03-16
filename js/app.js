024d9e07f9e8898fadef3346c20af9f77af7f0ce
hColleen <hcolleen@gmail.com>
2019-03-16 05:29:52 -0700

    create color array and set pseudo random selection

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
-//fibonacci array
+//repeat number arrays
+
 let fibonacciArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89], binaryArray = [2, 4, 8, 16, 32, 64, 128];
+
 //color array
+
 let fullColorArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 let colorArray = [];
-//select color without repeats
+let count = document.getElementById("numColors").value;
 
-//select random fibinachi number
+for (let i = 0; i < count; i++) {
+    colorArray.push(fullColorArray[i]);
+}
+
+//select color without repeats (tutorial from here: https://inteist.com/javascript-generate-pseudo-random-set/ )
+
+function numArr(limit) {
+    list = [];
+    for (let i = 0; i < limit; i++) {
+        list.push(i);
+    }
+    return list;
+}
+
+function shuffle(array) {
+    let i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+let getColors = shuffle(numArr(colorArray.length))
+
+//select random repeat numbers
 
 //display color and number
 
