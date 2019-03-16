ef74f6ef5a05952d2f0eb2a14255f3cc88683b0d
hColleen <hcolleen@gmail.com>
2019-03-16 04:40:26 -0700

    start building js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-//fibinachi array
-
+//fibonacci array
+let fibonacciArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89], binaryArray = [2, 4, 8, 16, 32, 64, 128];
 //color array
-
+let fullColorArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+let colorArray = [];
 //select color without repeats
 
 //select random fibinachi number
