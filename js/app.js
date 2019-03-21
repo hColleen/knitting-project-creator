d6d3e3f7d8f0678baef1a238f4f63574e7e11853
hColleen <hcolleen@gmail.com>
2019-03-21 02:56:29 -0700

    assign row/color count and add comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //gather form information (assistance from here https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio)
+//set up variables
 let form = document.querySelector('form');
 let output = [];
 let fibonacciArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89], binaryArray = [2, 4, 8, 16, 32, 64, 128];
@@ -6,15 +7,22 @@ let rowSelection = null;
 let colorCount = "";
 let repeatCount = "";
 
-form.addEventListener("submit", function(e){
+//on submit function
+form.addEventListener("submit", function (e) {
     let data = new FormData(form);
-    for (const entry of data){
+    for (const entry of data) {
+        //gather information from form
         output.push(entry[1]);
         console.log(output);
     };
+    //hide article, reveal print button
     document.querySelector('article').classList.add('hide');
     document.querySelector('button').classList.remove('hide');
-    if (output[2] === "binary"){
+    //assign colors and repeat values
+    colorCount = output[0];
+    repeatCount = output[1];
+    //select array and reset output array
+    if (output[2] === "binary") {
         rowSelection = binaryArray;
         output = [];
     } else if (output[2] === "fibonacci") {
@@ -52,7 +60,7 @@ function shuffle(array) {
 
 let rowArray = [];
 
-for(let i = 0; i < repeatCount; i++){
+for (let i = 0; i < repeatCount; i++) {
     rowArray.push(rowSelection[i]);
 }
 
